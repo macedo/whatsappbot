b391591bc728e2886d37bb2af693fb349c5cc06b
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2023-01-06 10:34:49 -0300

    refactor(server): improve server

--- main.go
+++ main.go
@@ -1,41 +1,69 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"context"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
 )
+
+var l *log.Logger
+
+var tmpl *template.Template
 
 type TmplData struct {
 	H1 string
 }
 
-func main() {
-	tmpl := template.Must(template.ParseFiles("index.html"))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := &TmplData{
-			H1: "Oie",
-		}
-
-		err := tmpl.Execute(w, data)
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-
-	addr := ":8080"
-
-	fmt.Printf("listening on %s", addr)
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Println("server closed")
-			os.Exit(1)
-		}
-		log.Fatal(err)
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	data := &TmplData{
+		H1: "Oie",
 	}
+
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Ooops", http.StatusBadRequest)
+		return
+	}
+}
+
+func main() {
+	l = log.New(os.Stdout, "server", log.LstdFlags)
+
+	tmpl = template.Must(template.ParseFiles("index.html"))
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomePage).Methods("GET")
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	signal.Notify(sigCh, syscall.SIGTERM)
+	sig := <-sigCh
+	l.Println("received terminate, graceful shutdown", sig)
+
+	timeotCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	srv.Shutdown((timeotCtx))
 }
