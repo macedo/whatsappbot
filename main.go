0dfbff08109752e636e2251372e66cdac2eb2c4d
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2023-01-06 09:45:43 -0300

    feat(server): hello world from http server

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-  fmt.Println("Oie")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Oie"))
+	})
+
+	addr := ":8080"
+
+	fmt.Printf("listening on %s", addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Println("server closed")
+			os.Exit(1)
+		}
+		log.Fatal(err)
+	}
 }
