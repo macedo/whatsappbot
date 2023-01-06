01a0c3ad5063aff335d1ffe28260305fecbd8199
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2023-01-06 10:09:42 -0300

    feat(templating): server render http template

--- main.go
+++ main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
+type TmplData struct {
+	H1 string
+}
+
 func main() {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Oie"))
+		data := &TmplData{
+			H1: "Oie",
+		}
+
+		err := tmpl.Execute(w, data)
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
 
 	addr := ":8080"
