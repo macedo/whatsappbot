04c86d27059f80ce2011227c8407065f07f69d42
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2023-01-10 09:27:50 -0300

    feat(conversation): parse message events and get reminder and datetime

--- go.mod
+++ go.mod
@@ -9,12 +9,21 @@ require (
 	go.mau.fi/whatsmeow v0.0.0-20230104001256-9d98dc9b5702
 )
 
-require github.com/lib/pq v1.10.7
+require (
+	github.com/lib/pq v1.10.7
+	github.com/olebedev/when v0.0.0-20221205223600-4d190b02b8d8
+)
+
+require (
+	github.com/AlekSi/pointer v1.0.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
 
 require (
 	filippo.io/edwards25519 v1.0.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16
 	go.mau.fi/libsignal v0.1.0 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/protobuf v1.28.1
 )
--- whatsapp/client.go
+++ whatsapp/client.go
@@ -1,30 +1,57 @@
 package whatsapp
 
 import (
+	"context"
 	"fmt"
+	"regexp"
+	"strings"
+	"time"
 
 	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/store"
+	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
 )
 
-// If you want to access the Client instance inside the event handler, the recommended way is to
-// wrap the whole handler in another struct:
-//
-//	type MyClient struct {
-//		WAClient *whatsmeow.Client
-//		eventHandlerID uint32
-//	}
-//
-//	func (mycli *MyClient) register() {
-//		mycli.eventHandlerID = mycli.WAClient.AddEventHandler(mycli.myEventHandler)
-//	}
-//
-//	func (mycli *MyClient) myEventHandler(evt interface{}) {
-//		// Handle event and access mycli.WAClient
-//	}
 type Client struct {
 	*whatsmeow.Client
+	eventHandlerID uint32
+}
+
+func (c *Client) registerHandler() {
+	c.eventHandlerID = c.AddEventHandler(c.eventHandler)
+}
+
+func (c *Client) eventHandler(evt interface{}) {
+	switch evtType := evt.(type) {
+	case *events.Message:
+		re := regexp.MustCompile(`^(remember me|me lembra de) ?(.*)?$`)
+		matches := re.FindStringSubmatch(evtType.Message.GetConversation())
+		if len(matches) == 0 {
+			l.Println("unhandled message - ", evtType.Message.GetConversation())
+			return
+		}
+
+		body := matches[0]
+
+		r, err := nlDTParser.Parse(body, time.Now())
+		if err != nil {
+			l.Fatal(err)
+		}
+
+		if r == nil {
+			c.SendMessage(context.TODO(), evtType.Info.Sender, &waProto.Message{
+				Conversation: proto.String("Desculpe, não entendi direito. Eu sou facilmente confundido. Talvez tente as palavras em uma ordem diferente. Isso geralmente funciona: me lembra de [o que] [quando]"),
+			})
+		} else {
+			reminder := strings.ReplaceAll(body, body[r.Index:r.Index+len(r.Text)], "")
+			reminder = strings.TrimSpace(reminder)
+
+			l.Printf("%s - %s", reminder, r.Time)
+		}
+	}
 }
 
 func NewClient(d *store.Device) *Client {
@@ -39,5 +66,10 @@ func NewClient(d *store.Device) *Client {
 	cliLog := waLog.Stdout(fmt.Sprintf("DEVICE-%s", id), logLevel, true)
 	waCli := whatsmeow.NewClient(d, cliLog)
 
-	return &Client{waCli}
+	client := &Client{
+		Client: waCli,
+	}
+	client.registerHandler()
+
+	return client
 }
